8d1b2c2de760a019f13cb3654c2f727de1f88073
Courtney Frey <66076696+speudusa@users.noreply.github.com>
2023-10-30 14:23:39 -0500

    All CRUD functions operational Woot

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,12 +1,11 @@
 import {Link} from 'react-router-dom';
 import { BsArrowLeft } from 'react-icons/bs';
 
-
-const BackButton = ({ destination='/'}) => {
+const BackButton = () => {
     return (
         <div className='flex'>
             <Link 
-                to={destination} 
+                to={'/'} 
                 className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
             >
                 <BsArrowLeft className='text-2xl'/>
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,9 +1,90 @@
-//import React from 'react'
+import {useState, useEffect} from 'react'; 
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const EditBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`http://localhost:5555/books/${id}`)
+    .then((response) => {
+      setTitle(response.data.title)
+      setAuthor(response.data.author)
+      setPublishYear(response.data.publishYear)
+      setLoading(false);
+    }).catch((error) => {
+      setLoading(false);
+      alert("An error has happened - see console for details");
+      console.log(error);
+    })
+  }, [])
+  
+  const handleEditBook = () => {
+    const bookData = {
+      title, 
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:5555/books/${id}`, bookData)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please check your console for more information')
+        console.log(error);
+      });
+  };
   return (
-    <div>EditBook</div>
-  )
+    <div className='p-4'>
+      <BackButton />
+       <h1 className='text=3xl my-4'>Edit Book</h1>
+      {loading ? <Spinner/> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-x1 w-[600px] p-4 mx-auto'> 
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input 
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+            />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input 
+            type='text'
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+            />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Year Published</label>
+          <input 
+            type='number'
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+            />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default EditBook
+export default EditBook;
